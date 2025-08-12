de38378d9aa1c0291f5c2bb000daff1861f048e5
ChinhDe <chinhde29@gmail.com>
2025-08-12 22:59:52 +0700

    settup and off spring security

--- src/main/java/vn/chinhde/phonestore/PhonestoreApplication.java
+++ src/main/java/vn/chinhde/phonestore/PhonestoreApplication.java
@@ -2,16 +2,13 @@ package vn.chinhde.phonestore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class PhonestoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhonestoreApplication.class, args);
-		
-
 	}
 
 }
-// 
